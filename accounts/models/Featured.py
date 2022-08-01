82de784c7544312e3dc38166fc1e8f0345b7ca0d
sifat3301 <nur.amin@brainstation-23.com>
2022-08-01 20:21:23 +0600

    admin setup

--- accounts/models/Featured.py
+++ accounts/models/Featured.py
@@ -3,8 +3,8 @@ from accounts.models.users import User
 
 
 class Featured(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_featured")
-    documents = models.FileField(upload_to="featured_photo")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_features")
+    documents = models.FileField(upload_to="featured_photos")
 
     def __str__(self):
         return f"{self.user.first_name}+ {self.user.last_name} featured"
--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from . models import Post, PostLike, Comment, CommentLike, Tag
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "id", "author_company", "author_user", "text"
+    )
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id", "author", "post", "text"
+    )
+
+
+@admin.register(PostLike)
+class PostLikeAdmin(admin.ModelAdmin):
+    list_display = (
+        "id", "user", "post"
+    )
+
+
+@admin.register(CommentLike)
+class CommentLikeAdmin(admin.ModelAdmin):
+    list_display = (
+        "id", "user", "comment"
+    )
+
+
+@admin.register(Tag)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id", "name"
+    )
+
