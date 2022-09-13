d41b0117bcd2df7e89e00076f76c99c094a3b5d1
Md. Nur Amin Sifat <nur.amin@brainstation-23.com>
2022-09-13 23:54:51 +0600

    custom pagination

--- common/pagination.py
+++ common/pagination.py
@@ -1,7 +1,4 @@
-from rest_framework import pagination
-from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
-from collections import OrderedDict
 
 
 class CustomPagination:
