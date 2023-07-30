48aab5a4b96c245f37ab456f2faac2bb5b6059b8
genesisblock3301 <nur15-1463@diu.edu.bd>
2023-07-30 07:35:05 +0600

    allowed host

--- configurations/settings.py
+++ configurations/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qdvw_7gvz(+$980f6#x(0(a)&xj^j*!@wpgo=erl8&9o=dhy7_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
